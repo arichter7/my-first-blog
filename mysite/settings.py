b24897c9da9ef860ce019c5e9fd3155c8499040a
arichter7 <aj.richter@sbcglobal.net>
2017-08-02 15:16:01 -0500

    fix settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,3 +118,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+ALLOWED_HOSTS = ['*']
+DATABASES = {
+    'default': {
+        'ENGINE':'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+    }
+}
